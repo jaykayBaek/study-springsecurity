8bdeb2230e1c0815def85cd7e94f4d3cad37af59
jaykayBaek <jaykaybaek@gmail.com>
2023-06-12 15:29:30 +0900

    학습: PasswordEncoder 학습

--- springsecsection4/src/main/java/com/eazybytes/controller/LoginController.java
+++ springsecsection4/src/main/java/com/eazybytes/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.eazybytes.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class LoginController {
     private final CustomerRepository customerRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody Customer customer){
+        Customer savedCustomer = null;
         ResponseEntity response = null;
-        Customer savedCustomer = customerRepository.save(customer);
-
         try {
+            String hashPassword = passwordEncoder.encode(customer.getPwd());
+            customer.setPwd(hashPassword);
+            savedCustomer = customerRepository.save(customer);
             if(savedCustomer.getId() > 0){
                 response = ResponseEntity
                         .status(HttpStatus.CREATED)
