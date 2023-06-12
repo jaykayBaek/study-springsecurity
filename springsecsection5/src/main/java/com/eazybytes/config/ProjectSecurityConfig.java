aeee8c07914746ae4ac822008dd8757b19d1f1c7
jaykayBaek <jaykaybaek@gmail.com>
2023-06-12 17:46:27 +0900

    학습: 커스텀 Authentication Provider 생성

---
+++ springsecsection5/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.eazybytes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class ProjectSecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf((csrf) -> csrf.disable())
+                        .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                        .requestMatchers("/notices", "/contact", "/register").permitAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+}
---
+++ springsecsection5/src/main/resources/sql/script.sql
@@ -0,0 +1,28 @@
+CREATE TABLE users (
+   id INT NOT NULL AUTO_INCREMENT,
+   username VARCHAR(45) NOT NULL,
+   password VARCHAR(45) NOT NULL,
+   enabled INT NOT NULL,
+   PRIMARY KEY(id)
+);
+
+CREATE TABLE authorities (
+     id INT NOT NULL AUTO_INCREMENT,
+     username VARCHAR(45) NOT NULL,
+     authority VARCHAR(45) NOT NULL,
+     PRIMARY KEY(id)
+);
+
+INSERT IGONORE INTO `users` VALUES (NULL, 'happy', '1234', '1');
+INSERT IGONORE INTO `authorities` VALUES (NULL, 'happy', 'write');
+
+CREATE TABLE `customer`
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(45) NOT NULL,
+    `pwd` VARCHAR(200) NOT NULL,
+    `role` VARCHAR(45) NOT NULL,
+    PRIMARY KEY(`id`)
+);
+
+INSERT INTO `customer` (`email`, `pwd`, `role`)
+VALUES('test@gmail.com', '1234', 'admin');
